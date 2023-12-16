198288dab72dd656379dc367437ca00a72aa8cce
ChaseHarding <chaseharding965@gmail.com>
2023-12-16 14:04:06 -0800

    game logic in js for moles appearing and disappearing, started score logic and reset

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
+const holes = document.querySelectorAll('.hole');
+
 let score = 0;
 
+//function for randome hole selection
+function randomHole() {
+ const index = Math.floor(Math.random() * holes.length)
+ return holes[index];
+}
+//function for making mole appear
+function molePeek() {
+//this should genereate a random time for the mole to pop up
+const time = Math.random() * 1000 + 500; //random timing
+const hole = randomHole();
+hole.classList.add('up'); //adding 'up' to the hole to make the mole pop up
+setTimeout(()=> {
+    hole.classList.remove('up'); //timeout to make the mole disappear after a random time
+    if (!timeUp) molePeek();
+}, time);
+}
+
+//function for whacking the mole
+function bonk(e) {
+    if (!e.isTrusted) return;
+
+    //score increase!!
+    score++;
+
+    //now hide the mole after getting hit
+    this.classList.remove('up');
+
+}
+
+//event listeners for mouse clicking
+//even listener for all holes, calling that bonk function
+holes.forEach(hole => hole.addEventListener('click', bonk));
+
+//timer for game to end
+let timeUp = false;
+
+//function for game start
+function startGame() {
+    //reset the score and game status 
+    score = 0
+    timeUp = false
+}
